3171512cc994c886ed91c0f9ab6473b2da41dd39
AGHproject13 <agehaproject13@gmail.com>
2023-03-21 11:55:29 +0700

    Add operator function
    Add event listener to operator buttons so when a pair of numbers already exists, then do operate function to do the calculation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const clearButton = document.querySelector(".buttons .clear-delete-row .clear");
 const numbers = document.querySelectorAll(".buttons .row .number");
 const operators = document.querySelectorAll(".buttons .row .operator");
 
+let result;
 let firstNumber;
 let secondNumber;
 let operator;
@@ -12,17 +13,20 @@ clearButton.addEventListener("click", clearDisplay);
 numbers.forEach((num) => {
   num.addEventListener("click", addNumber);
 });
-// operators.forEach((op) => {
-//   op.addEventListener("click", addOperator);
-// });
+operators.forEach((op) => {
+  op.addEventListener("click", addOperator);
+});
 
-// Functions
+/* Functions */
 function updateDisplay() {
   display.innerText = result;
 }
 
 function clearDisplay() {
   result = "0";
+  firstNumber = "0";
+  operator = null;
+  secondNumber = null;
   updateDisplay();
 }
 
@@ -32,10 +36,30 @@ function addNumber() {
   } else {
     result += this.innerText;
   }
+
+  // if no operator yet, add to first number
+  if (!operator) firstNumber = result;
+  // If an operator already chosen, then add to second number
+  else if (!secondNumber) secondNumber = this.innerText;
+  else secondNumber += this.innerText;
+  console.log(firstNumber + " " + secondNumber);
   updateDisplay();
 }
 
-// function addOperator() {}
+function addOperator() {
+  // If a pair of numbers already exists
+  if (operator && secondNumber) {
+    result = operate(operator, +firstNumber, +secondNumber);
+    firstNumber = "" + result;
+    secondNumber = null;
+    console.log(result);
+  }
+
+  result += this.innerText;
+  operator = this.innerText;
+  console.log(operator);
+  updateDisplay();
+}
 
 function add(num1, num2) {
   return num1 + num2;
@@ -56,6 +80,6 @@ function divide(num1, num2) {
 function operate(symbol, num1, num2) {
   if (symbol === "+") return add(num1, num2);
   if (symbol === "-") return subtract(num1, num2);
-  if (symbol === "*") return multiply(num1, num2);
+  if (symbol === "×") return multiply(num1, num2);
   else return divide(num1, num2);
 }
